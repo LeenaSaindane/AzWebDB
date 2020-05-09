2322aa0c95590f5f0ec4580731ae9899c769e05f
LeenaSaindane <58260761+LeenaSaindane@users.noreply.github.com>
2020-05-09 15:44:30 +0200

    Add files via upload

---
+++ DB.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace AzWebDB
+{
+    public partial class DB : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!IsPostBack)
+
+            {
+
+                //DisplayRecord();
+                string connStr = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+
+                SqlConnection con = new SqlConnection(connStr);
+
+                SqlDataAdapter Adp = new SqlDataAdapter("select ename from dbo.emp where empno=1001", con);
+
+                DataTable Dt = new DataTable();
+
+                Adp.Fill(Dt);
+
+                gvEmp.DataSource = Dt;
+
+                gvEmp.DataBind();
+
+                con.Close();
+
+
+
+            }
+
+        }
+
+      
+    }
+}
